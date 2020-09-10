03a53a2024b70dd5bd98bb3e353f50618ecb6145
Ryota-1 <stream.of.tears@icloud.com>
2020-09-10 22:10:54 +0900

    ユーザー、管理者、ジャンル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  	sessions: 'admins/sessions'
+  }
+
+  namespace :admins do
+  	resources :genres
+  end
+
+  devise_for :users, controllers: {
+  	sessions: 'users/sessions',
+  	passwords: 'users/passwords',
+    registrations: 'users/registrations',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
